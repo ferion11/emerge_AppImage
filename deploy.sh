058ca3551cae7efee61b3ed1726fd0f2a5c759d8
DanielDevBR <dados.linux@gmail.com>
2020-10-04 18:51:33 -0300

    mount all var, more mount like run, dev/pts, kernel src and modules

--- deploy.sh
+++ deploy.sh
@@ -37,12 +37,8 @@ EOF
 
 chmod +x "${WORKDIR}"/emerge.sh
 
-mkdir -p ${WORKDIR}/var/db/repos
-mkdir -p ${WORKDIR}/var/cache/distfiles
-mkdir -p ${WORKDIR}/var/cache/binpkgs
-
-rm -rf ${WORKDIR}/var/log
-mkdir -p ${WORKDIR}/var/log
+rm -rf ${WORKDIR}/var
+mkdir -p ${WORKDIR}/var
 #=================================================
 
 wget -nv -c "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage" -O  appimagetool.AppImage
